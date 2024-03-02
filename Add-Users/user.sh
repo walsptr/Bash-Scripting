31b29d7f4c8e82a68cceb0e9c4079aadc46a2bb7
walsptr <syawal.als.com@gmail.com>
2024-03-02 20:10:38 +0700

    update adduser

--- Add-Users/user.sh
+++ Add-Users/user.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #set -x
 MY_INPUT='/path/to/user.csv'
-declare -a SURNAME
 declare -a NAME
+declare -a SURNAME
 declare -a USERNAME
 declare -a GROUP
 declare -a PASSWORD
 while IFS=, read -r COL1 COL2 COL3 COL4 COL5 TRASH;
 do
-        SURNAME+=("$COL1")
-        NAME+=("$COL2")
+        NAME+=("$COL1")
+        SURNAME+=("$COL2")
         USERNAME+=("$COL3")
         GROUP+=("$COL4")
         PASSWORD+=("$COL5")
