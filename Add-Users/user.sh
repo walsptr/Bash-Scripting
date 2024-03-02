4fb1104704b5c984c20f560cd2c82166918128f4
walsptr <syawal.als.com@gmail.com>
2024-03-02 20:07:59 +0700

    adduser

---
+++ Add-Users/user.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#set -x
+MY_INPUT='/path/to/user.csv'
+declare -a SURNAME
+declare -a NAME
+declare -a USERNAME
+declare -a GROUP
+declare -a PASSWORD
+while IFS=, read -r COL1 COL2 COL3 COL4 COL5 TRASH;
+do
+        SURNAME+=("$COL1")
+        NAME+=("$COL2")
+        USERNAME+=("$COL3")
+        GROUP+=("$COL4")
+        PASSWORD+=("$COL5")
+done <"$MY_INPUT"
+for index in "${!USERNAME[@]}"; do
+        groupadd "${GROUP[$index]}";
+        useradd -G "${GROUP[$index]}" -d "/home/${USERNAME[$index]}" -s /bin/bash -p "$(echo "${PASSWORD[$index]}" | openssl passwd -1 -stdin)" "${USERNAME[$index]}"
+done
